a1016f9792d8c2b626d4e7621cc94c9ccc54f694
Krasivaya <51264308+Krasivaya@users.noreply.github.com>
2019-08-09 11:49:04 +0200

    ft(signin-api): user should be able to sign in (#3)

--- server/server/routes/index.js
+++ server/server/routes/index.js
@@ -1,11 +1,15 @@
 import express from 'express';
 import Signup from '../controllers/signupController';
-import validate from '../helpers/validation'
+import Signin from '../controllers/signinController';
+import validate from '../helpers/signup_validation';
+import validateSignin from '../helpers/signin_validation'
 
 const router = express.Router();
 
 
 router.post('/auth/signup', validate, Signup.signup);
+
+router.post('/auth/signin', validateSignin, Signin.signin);
 
 export default router;
    
